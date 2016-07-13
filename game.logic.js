5c0829cb2cf12ba43a65acfe257aa80ee2417a4f
yolomachine <thisisbanana@hotmail.com>
2016-07-13 21:58:45 +0900

    Refactoring

--- game.logic.js
+++ game.logic.js
@@ -1,21 +1,12 @@
-const radius = 17, EPS = 0.000001;
+const radius = 17, EPS = 1e-16;
 
-if (gameSession === undefined) var gameSession = {};
-
-gameSession.point = function(x, y, radius) {
-    this.x = x;
-    this.y = y;
-}
-
-gameSession.line = function(p1, p2) {
-    this.beginPoint = p1;
-    this.endPoint = p2;
-    this.beginCoords = gameSession.points[this.beginPoint];
-    this.endCoords = gameSession.points[this.endPoint];
-    this.A = this.endCoords.y - this.beginCoords.y;
-    this.B = this.beginCoords.x - this.endCoords.x;
-    this.C = -this.A * (this.beginCoords.x) - this.B * (this.beginCoords.y);
-    this.intersecting = false;
+function makeLine(point1, point2) {
+    let beginPoint = { x: point1.x, y: point1.y };
+    let endPoint = { x: point2.x, y: point2.y };
+    let A = point2.y - point1.y;
+    let B = point1.x - point2.x;
+    let C = -A * point1.x - B * point1.y;
+    return { A: A, B: B, C: C, beginPoint: beginPoint, endPoint: endPoint };
 }
 
 function isInSegment(a, b, c) {
@@ -24,20 +15,17 @@ function isInSegment(a, b, c) {
     return (a + r < b && b < c - r) || (c + r < b && b < a - r);
 }
 
-function isIntersecting(a, b) {
-    let delta = a.A * b.B - b.A * a.B;
-    if (Math.abs(delta) < 1e-10) return false;
-    let delta_x = -b.B * a.C + a.B * b.C;
-    let delta_y = -a.A * b.C + b.A * a.C;
-    let x = delta_x / delta;
-    let y = delta_y / delta;
-
-    if ((isInSegment(a.beginCoords.x, x, a.endCoords.x) || isInSegment(a.beginCoords.y, y, a.endCoords.y)) &&
-        (isInSegment(b.beginCoords.x, x, b.endCoords.x) || isInSegment(b.beginCoords.y, y, b.endCoords.y))) {
-        return { x: x, y: y };
-    }
+function intersectionPoint(line1, line2) {
+    let det = line1.A * line2.B - line2.A * line1.B;
+    if (Math.abs(det) < 1e-10) return false;
+    let detX = -line2.B * line1.C + line1.B * line2.C;
+    let detY = -line1.A * line2.C + line2.A * line1.C;
+    let x = detX / det;
+    let y = detY / det;
+    if ((isInSegment(line1.beginPoint.x, x, line1.endPoint.x) ||
+         isInSegment(line1.beginPoint.y, y, line1.endPoint.y)) &&
+        (isInSegment(line2.beginPoint.x, x, line2.endPoint.x) ||
+         isInSegment(line2.beginPoint.y, y, line2.endPoint.y)))
+            return { x: x, y: y};
     return false;
 }
-
-//TODO SOLVER (firebase)
-//TODO RESULTS TABLE
--- game.scenery.js
+++ game.scenery.js
@@ -1,156 +1,160 @@
 const fieldWidth = 1000, fieldHeight = 800;
-var canvas, ctx, cursorPosX, cursorPosY, selectedPoint, ps, count = 0;
+var canvas, ctx, cursorPosX, cursorPosY,
+    selectedPoint, intersectionPoints,
+    points, edges, count, currentLevel = minLevel;
 
-if (gameSession === undefined) var gameSession = {};
-gameSession.currentLevel = minLevel;
+window.onload = initialize;
 
 function initialize() {
-    gameSession.createLayout();
-    cursorPosX = 0; cursorPosY = 0;
+    field.createLayout();
+    cursorPosX = cursorPosY = 0;
     canvas = document.createElement("canvas");
     ctx = canvas.getContext("2d");
     canvas.width = fieldWidth;
     canvas.height = fieldHeight;
     document.body.appendChild(canvas);
-    canvas.addEventListener('mousemove',  mouseMove,  false);
-    canvas.addEventListener('mousedown',  mouseDown,  false);
-    canvas.addEventListener('mouseup',    mouseUp,    false);
+    canvas.addEventListener('mousemove',  mouse.move,  false);
+    canvas.addEventListener('mousedown',  mouse.down,  false);
+    canvas.addEventListener('mouseup',    mouse.up,    false);
     draw();
-}
-
-function movePoint() {
-    gameSession.points[selectedPoint].x = cursorPosX;
-    gameSession.points[selectedPoint].y = cursorPosY;
-}
-
-function selectPoint() {
-    selectedPoint = undefined;
-    let x, y, xDis, yDis, dis, minDis = Math.PI * (radius * radius) / 2;
-    for (let i = 0; i < gameSession.points.length; ++i) {
-        x = gameSession.points[i].x;
-        y = gameSession.points[i].y;
-        xDis = x - cursorPosX;
-        yDis = y - cursorPosY;
-        dis  = xDis * xDis + yDis * yDis;
-        if (dis <= minDis) { minDis = dis; selectedPoint = i };
-    }
 }
 
 function draw() {
     ctx.clearRect(0, 0, fieldWidth, fieldHeight);
     let x, y;
     ctx.lineWidth = 2;
-    ps = [];
-    for (let i = 0; i < gameSession.edges.length; ++i) {
+    intersectionPoints = [];
+    for (let i = 0; i < edges.length; ++i) {
         count = 0;
-        gameSession.edges[i].beginCoords = gameSession.points[gameSession.edges[i].beginPoint];
-        gameSession.edges[i].endCoords = gameSession.points[gameSession.edges[i].endPoint];
-        gameSession.edges[i].A = gameSession.edges[i].endCoords.y - gameSession.edges[i].beginCoords.y;
-        gameSession.edges[i].B = gameSession.edges[i].beginCoords.x - gameSession.edges[i].endCoords.x;
-        gameSession.edges[i].C = -gameSession.edges[i].A * (gameSession.edges[i].beginCoords.x) -
-                                  gameSession.edges[i].B * (gameSession.edges[i].beginCoords.y);
-        for (let j = 0; j < gameSession.edges.length; ++j) {
-            gameSession.edges[j].beginCoords = gameSession.points[gameSession.edges[j].beginPoint];
-            gameSession.edges[j].endCoords = gameSession.points[gameSession.edges[j].endPoint];
-            gameSession.edges[j].A = gameSession.edges[j].endCoords.y - gameSession.edges[j].beginCoords.y;
-            gameSession.edges[j].B = gameSession.edges[j].beginCoords.x - gameSession.edges[j].endCoords.x;
-            gameSession.edges[j].C = -gameSession.edges[j].A * (gameSession.edges[j].beginCoords.x) -
-                                      gameSession.edges[j].B * (gameSession.edges[j].beginCoords.y);
-            let curEdge = gameSession.edges[i], nextEdge = gameSession.edges[j];
-            if (curEdge == nextEdge) continue;
-            let p = isIntersecting(curEdge, nextEdge);
+        let curBegin = points[edges[i].beginPoint],
+            curEnd = points[edges[i].endPoint];
+        for (let j = 0; j < edges.length; ++j) {
+            let nextBegin = points[edges[j].beginPoint],
+                nextEnd = points[edges[j].endPoint];
+            let curLine  = makeLine(curBegin, curEnd),
+                nextLine = makeLine(nextBegin, nextEnd);
+            let curEdge  = edges[i],
+                nextEdge = edges[j];
+            if (curLine == nextLine) continue;
+            let p = intersectionPoint(curLine, nextLine);
             if (p !== false) {
                 ++count;
                 curEdge.intersecting = nextEdge.intersecting = true;
-                ps.push(p);
+                intersectionPoints.push(p);
                 break;
             }
             if (count == 0) curEdge.intersecting = false;
         }
     }
-    for (let i = 0, t0, t1; i < gameSession.edges.length; ++i) {
-        t0 = gameSession.points[gameSession.edges[i].beginPoint];
-        t1 = gameSession.points[gameSession.edges[i].endPoint];
+    for (let i = 0, t0, t1; i < edges.length; ++i) {
+        t0 = points[edges[i].beginPoint];
+        t1 = points[edges[i].endPoint];
         ctx.lineJoin = ctx.lineCap = 'round';
-        ctx.shadowBlur = 5;
+        ctx.shadowBlur = 3;
         ctx.shadowColor = 'rgb(0, 0, 0)';
         ctx.strokeStyle = "green";
-        if (gameSession.edges[i].intersecting) ctx.strokeStyle = "red";
+        if (edges[i].intersecting) ctx.strokeStyle = "red";
         ctx.beginPath();
         ctx.moveTo(t0.x, t0.y);
         ctx.lineTo(t1.x, t1.y);
         ctx.closePath();
         ctx.stroke();
     }
-    for (let i = 0; i < ps.length; ++i) {
-        drawPointPath(5, ps[i].x, ps[i].y);
+    for (let i = 0; i < intersectionPoints.length; ++i) {
+        field.drawPointPath(5, intersectionPoints[i].x, intersectionPoints[i].y);
         ctx.fillStyle = 'blue';
         ctx.fill();
     }
-    for (let i = 0; i < gameSession.points.length; ++i) {
-        x = gameSession.points[i].x;
-        y = gameSession.points[i].y;
-        drawPointPath(radius, x, y);
+    for (let i = 0; i < points.length; ++i) {
+        x = points[i].x;
+        y = points[i].y;
+        field.drawPointPath(radius, x, y);
         ctx.fillStyle = '#333';
         ctx.fill();
     }
     window.requestAnimationFrame(draw);
-}
-
-function drawPointPath(r, x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI*2, true);
-    ctx.closePath();
-}
-
-function getMouseCoords(event) {
-    cursorPosX = event.pageX - canvas.offsetLeft;
-    cursorPosY = event.pageY - canvas.offsetTop;
-    if (cursorPosX > fieldWidth - radius) cursorPosX = fieldWidth - radius;
-    else if (cursorPosX < radius) cursorPosX = radius;
-    if (cursorPosY > fieldHeight - radius) cursorPosY = fieldHeight - radius;
-    else if (cursorPosY < radius) cursorPosY = radius;
-}
-
-function mouseDown(event) {
-    getMouseCoords(event);
-    selectPoint();
-    movePoint();
-}
-
-function mouseUp(event) {
-    selectedPoint = undefined;
-    if (ps.length == 0) gameSession.changeLevel(1);
-}
-
-function mouseMove(event) {
-    getMouseCoords(event);
-    if (selectedPoint != undefined) movePoint();
-}
-
-function clear(obj) {
-    while (obj.lastChild) obj.removeChild(obj.lastChild);
-}
-
-gameSession.changeLevel = function(inc) {
-    if (gameSession.currentLevel + inc >= minLevel && gameSession.currentLevel + inc <= maxLevel)
-        gameSession.currentLevel += inc;
-    gameSession.createLayout();
-}
-
-gameSession.createLayout = function() {
-    gameSession.points = [];
-    gameSession.edges  = [];
-    let level = presetLevels[gameSession.currentLevel];
-    for (let i = 0; i < level.points.length; ++i)
-        gameSession.points.push(new gameSession.point(level.points[i].x, level.points[i].y, radius));
-    for (let i = 0; i < level.edges.length; ++i)
-        for (let j = 0; j < level.edges[i].length - 1; ++j)
-            gameSession.edges.push(new gameSession.line(level.edges[i][j], level.edges[i][j + 1]));
 };
 
-gameSession.generateLayout = function(level) {
-
+function Field() {
+    this.clear = function(obj) {
+        while (obj.lastChild) obj.removeChild(obj.lastChild);
+    }
+    this.selectPoint = function() {
+        selectedPoint = undefined;
+        let x, y, xDis, yDis, dis, minDis = Math.PI * (radius * radius) / 2;
+        for (let i = 0; i < points.length; ++i) {
+            x = points[i].x;
+            y = points[i].y;
+            xDis = x - cursorPosX;
+            yDis = y - cursorPosY;
+            dis  = xDis * xDis + yDis * yDis;
+            if (dis <= minDis) { minDis = dis; selectedPoint = i };
+        }
+    }
+    this.movePoint = function() {
+        points[selectedPoint].x = cursorPosX;
+        points[selectedPoint].y = cursorPosY;
+        console.log(points);
+    }
+    this.drawPointPath = function(r, x, y) {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI*2, true);
+        ctx.closePath();
+    }
+    this.createLayout = function() {
+        points = [];
+        edges  = [];
+        let level = presetLevels[currentLevel];
+        for (let i = 0; i < level.points.length; ++i) {
+            let point = {};
+            point.x = level.points[i].x;
+            point.y = level.points[i].y;
+            points.push(point);
+        }
+        console.log(points);
+        for (let i = 0; i < level.edges.length; ++i)
+            for (let j = 0; j < level.edges[i].length - 1; ++j) {
+                let edge  = {};
+                edge.beginPoint = level.edges[i][j];
+                edge.endPoint = level.edges[i][j + 1];
+                edge.intersecting = false;
+                edges.push(edge);
+            }
+        console.log(points, edges);
+    }
+    this.changeLevel = function(inc) {
+        if (currentLevel + inc >= minLevel &&
+            currentLevel + inc <= maxLevel)
+            currentLevel += inc;
+        field.createLayout();
+    }
 }
 
-window.onload = initialize;
+function Mouse() {
+    this.move = function(event) {
+        mouse.getCoords(event);
+        if (selectedPoint != undefined) field.movePoint();
+    }
+    this.down = function(event) {
+        mouse.getCoords(event);
+        field.selectPoint();
+        field.movePoint();
+    }
+    this.up = function(event) {
+        selectedPoint = undefined;
+        if (intersectionPoints.length == 0 && currentLevel != maxLevel)
+            field.changeLevel(1);
+    }
+    this.getCoords = function(event) {
+        cursorPosX = event.pageX - canvas.offsetLeft;
+        cursorPosY = event.pageY - canvas.offsetTop;
+        if (cursorPosX > fieldWidth - radius) cursorPosX = fieldWidth - radius;
+        else if (cursorPosX < radius) cursorPosX = radius;
+        if (cursorPosY > fieldHeight - radius) cursorPosY = fieldHeight - radius;
+        else if (cursorPosY < radius) cursorPosY = radius;
+    }
+
+};
+
+var mouse = new Mouse();
+var field = new Field();
